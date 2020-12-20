eac09e1f6ddccb863b70fd6bb06ef4063f62fc8c
gwynbleidd10 <gwynbleidd10@gmail.com>
2020-12-20 16:52:48 +0900

    iter

--- monitor.js
+++ monitor.js
@@ -98,6 +98,7 @@ const check = async (is) => {
 }
 
 const start = async () => {
+    console.log('Iter')
     for (const is of urls) {
         if (is.type == process.env.type) {
             await check(is)
